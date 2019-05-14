fef4276dc8c8d3ed7d5c65faae465a088c35d2bc
Weigel <jesse.weigel@dcsg.com>
2019-05-14 14:50:55 -0400

    Updates

---
+++ precache-manifest.cec7b735b2e2c3d3f74e7e73c9108c32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "623a9105893d32aca15ab8d48a292f3b",
+    "url": "/one-codebase-to-rule-them-all/static/media/twitter-mobile.623a9105.png"
+  },
+  {
+    "revision": "d42bd1a5f10a8baa23ae5de62dd1ac54",
+    "url": "/one-codebase-to-rule-them-all/static/media/mls.d42bd1a5.png"
+  },
+  {
+    "revision": "552a656638baed2295f1ba61a7e03ed6",
+    "url": "/one-codebase-to-rule-them-all/static/media/dataCamp.552a6566.png"
+  },
+  {
+    "revision": "f481bdd12d45b89c1983",
+    "url": "/one-codebase-to-rule-them-all/static/js/runtime~main.f481bdd1.js"
+  },
+  {
+    "revision": "2d85c7fef4bd295a377b",
+    "url": "/one-codebase-to-rule-them-all/static/js/main.2d85c7fe.chunk.js"
+  },
+  {
+    "revision": "f89b8519ce89947eaac4",
+    "url": "/one-codebase-to-rule-them-all/static/js/1.f89b8519.chunk.js"
+  },
+  {
+    "revision": "f89b8519ce89947eaac4",
+    "url": "/one-codebase-to-rule-them-all/static/css/1.885d66ba.chunk.css"
+  },
+  {
+    "revision": "a423726032de7677d2de00318924d4d3",
+    "url": "/one-codebase-to-rule-them-all/index.html"
+  }
+];
